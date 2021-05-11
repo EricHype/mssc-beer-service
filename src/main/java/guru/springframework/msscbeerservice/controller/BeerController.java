796829a77fa90497d2ab4511d731a6f3b6d7902e
EricHype <eheitmuller@gmail.com>
2021-05-11 16:46:19 -0700

    Adding skeleton BeerController

---
+++ src/main/java/guru/springframework/msscbeerservice/controller/BeerController.java
@@ -0,0 +1,29 @@
+package guru.springframework.msscbeerservice.controller;
+
+import guru.springframework.msscbeerservice.web_model.BeerDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/beer")
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDTO> getBeerById(@PathVariable("beerId") UUID beerId) {
+
+        return new ResponseEntity<>(BeerDTO.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<BeerDTO> saveNewBeer(@RequestBody BeerDTO beerDTO) {
+        return new ResponseEntity<>(BeerDTO.builder().build(), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeer(@PathVariable("beerId") UUID beerId, @RequestBody BeerDTO beerDTO) {
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
